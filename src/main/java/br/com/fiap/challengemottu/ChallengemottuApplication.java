5948683172b101a324be7a1ae9db681890fc6515
anacarolcg0 <anacarolcg0@gmail.com>
2025-09-16 22:23:39 -0300

    iniciando...

---
+++ src/main/java/br/com/fiap/challengemottu/ChallengemottuApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengemottu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengemottuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengemottuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/challengemottu/ChallengemottuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengemottu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengemottuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
